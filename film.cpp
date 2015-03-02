34ed76ad257bfa0acf32c5d32d026a81de641cce
IamKorra13 <a.collins05@berkeley.edu>
2015-03-01 19:39:51 -0800

    changes to film from Saturdays meetup

--- film.cpp
+++ film.cpp
@@ -1,9 +1,21 @@
 #include "film.h"
+/* You will make one Film class for each rendered image.
+** The Film object will take in the dimentions of the image.
+** It will then make a 2D array buckets based on the dimentions.
+** At each point it will store the color vector.*/
 
-void takeSamples(Vector color, int x, int y){
-
+Film::Film(int x, int y) {
+	dim_x = x;
+	dim_y = y;
 }
 
-void writeColor(Vector color, int x, int y){
+int buckets[dim_x][dim_y];
+
+void takeSamples(int sample, int x, int y){
+	//make an array to store the pixels
+	buckets[x][y].push_back(sample);
+}
+//display sample color
+void writeSamples(Vector color, int x, int y){
 
 }
--- film.h
+++ film.h
@@ -4,22 +4,28 @@
 #include <iostream>
 #include <cmath>
 #include "vector.h"
+//#include "sample.h"
 
 using namespace std;
 
 class Film {
 	public:
-	  	
-	  	//This will take in the color vector of the pixel
+	  	int dim_x;
+	  	int dim_y;
+	  	//Samples will have an x and y coordinate and should have a color
+	  	//This will store the samples in 2D array.
+	  	Film(int, int);
+
+// holds all different samples per pixel
 	  	void takeSamples(Vector, int, int);
 	  	
 	  	/*This will write that color vVctor (bucket) to the pixel in the image file
 	  	The ints are the positons of the pixels the color will be set at.*/
-	  	void writeColor(Vector, int, int);
+	  	void writeSamples(Vector, int, int);
 
 	  	//Possible 2D "Bucket" array
 	  	//array[x][y] = color_vector
-	  	int buckets;
+	  	<int>buckets;
 };
 
 #endif
